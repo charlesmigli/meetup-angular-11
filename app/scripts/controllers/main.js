7026f44f85d6f41e27b8366043aaecff4288b07d
Charles Miglietti <charles.miglietti@yahoo.fr>
2014-05-26 16:28:44 +0200

    bar simple

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,11 @@
 
 angular.module('talkAngularApp')
   .controller('MainCtrl', function ($scope) {
+
+		/*********** PART ONE ******************/
 		$scope.svgWidth = 570;
-		$scope.svgHeight = 700;
+		$scope.svgHeight = 400;
 		$scope.svgScale = 1;
-
-
 		$scope.wheelSize = 30.;
 		$scope.carLength = 250;
 		$scope.carTopHeight = 20;
@@ -21,17 +21,46 @@ angular.module('talkAngularApp')
 		$scope.wheelXPosition = function() {
 			return {rear: 0.2*$scope.carLength, top:0.8*$scope.carLength}
 		};
+		//var count = 0;
+		//var interval = setInterval(function() {
+			//count++;
+			//if (count == 100) {
+				//clearInterval(interval);
+			//}
+			//$scope.$apply(function(){
+				//$scope.wheelSize = $scope.wheelSize + 0.2;
+			//});
+		//}, 10);
 
-		var count = 0;
-		var interval = setInterval(function() {
-			count++;
-			if (count == 100) {
-				clearInterval(interval);
-			}
-			$scope.$apply(function(){
-				$scope.wheelSize = $scope.wheelSize + 0.2;
-			});
-		}, 10);
+		/*********** PART TWO ******************/
+		$scope.barHeight = 10;
+		$scope.barOffset = 30;
+		$scope.barchartWidth = 570;
+		$scope.barchartHeight = 600;
+
+
+		$scope.data = [ { value: 0.586721 , dept:'Hautes-Pyrénées'}
+		, { value: 0.341481 , dept:'Tarn-et-Garonne'}
+		, { value: 0.327363 , dept:'Paris'}
+		, { value: 0.285993 , dept:'Haut-Rhin'}
+		, { value: 0.267445 , dept:'Marne'}
+		, { value: 0.266246 , dept:'Tarn'}
+		, { value: 0.264109 , dept:'Lot-et-Garonne'}
+		, { value: 0.246466 , dept:'Vaucluse'}
+		, { value: 0.245386 , dept:'Territoire de Belfort'}
+		, { value: 0.238322 , dept:'Nièvre'}
+		];
+
+
+		$scope.barWidth = function (value) {
+			var MAX_BAR_WIDTH = 300;
+			return  value / 0.5868 * MAX_BAR_WIDTH;
+		}
+		$scope.offsetGroup = function(index) {
+			return 'translate(0, '+ (index+1) *( $scope.barHeight + $scope.barOffset)+')';
+
+		};
+
 
 
   });
