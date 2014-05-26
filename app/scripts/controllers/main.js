1de011b31197ac51e985526381c2f72e757bd7c9
Charles Miglietti <charles.miglietti@yahoo.fr>
2014-05-26 14:47:13 +0200

    car modeling

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,8 +2,24 @@
 
 angular.module('talkAngularApp')
   .controller('MainCtrl', function ($scope) {
-		$scope.svgWidth = 800;
+		$scope.svgWidth = 570;
 		$scope.svgHeight = 700;
-		$scope.svgScale = 2;
+		$scope.svgScale = 1;
+
+
+		$scope.wheelSize = 30;
+		$scope.carLength = 250;
+		$scope.carTopHeight = 20;
+		$scope.carHeight = 60;
+		$scope.carBrand = 'Jaguar';
+		$scope.carTopXPosition = function() {
+			return 0.25* $scope.carLength;
+		}
+		$scope.wheelYPosition = function() {
+			return $scope.carTopHeight+ $scope.carHeight;
+		};
+		$scope.wheelXPosition = function() {
+			return {rear: 0.2*$scope.carLength, top:0.8*$scope.carLength}
+		};
 
   });
